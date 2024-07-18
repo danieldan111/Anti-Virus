fa5f566d6ab4b45a4b47627643d583882bff89c5
danieldan111 <qpwnqpwn113@gmail.com>
2024-07-18 14:56:35 +0300

    go thorugh all the files

--- Anti Virus.py
+++ Anti Virus.py
@@ -1,12 +1,18 @@
 import os
+import requests
 
-def folder_search(path, folder):
+def folder_search(path):
     for file in os.listdir(path):
-        print(file + f"  --- {folder}")
-        if (os.path.splitext(file)[1] == ""):
-            folder_search(path + "/" + file, file)
+        print(file)
+        try:
+            if (os.path.splitext(file)[1] == ""):
+                folder_search(path + "/" + file)
+        except NotADirectoryError:
+            pass
+            
 
 
 
 first = "C:/Users/ADMIN/Desktop/Harel/BoazSharabis/Img"
-folder_search(first, "main")
+first = "C:/Users/ADMIN/Downloads"
+folder_search(first)
