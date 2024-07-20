0b8102c27abd9570268aaa1939b00a8751429f04
danieldan111 <qpwnqpwn113@gmail.com>
2024-07-20 20:24:58 +0300

    Added proggress bar

--- AntiVirus.py
+++ AntiVirus.py
@@ -5,8 +5,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image #pip install pillow
-
-
+from tkinter import ttk
 
 
 
@@ -31,28 +30,71 @@ window.config(background="#161625")
 label = Label(window, text="Daniel's Anti Virus", font=('Arial', 25),bg='#161625', fg='White')
 label.pack()
 
+
 def page_load():
+    def checkAPI():
+        url = "https://www.virustotal.com/api/v3/files/59fce88da57e076536ccece2cd0b005991450b472d5de6de4f554d3ea1452ed5"
+
+        headers = {
+            "accept": "application/json",
+            "x-apikey": f"{key}"
+        }
+
+        response = requests.get(url, headers=headers)
+        message = response.text[response.text.find("message")::]
+        message = message[0: message.find("\n")]
+        if "Wrong API key" in message:
+            clearKey()
+            key_entry.insert(0,"Error: API key is not Valid")
+            return 1
+        return 0
+    def checkPath():
+        if (not os.path.isdir(path_entry.get())):
+            clearPath()
+            path_entry.insert(0,"Error: Invalid path")
+            return 1
+        path_entry.config(state=DISABLED)
+        return 0
+
     def keySet():
         global key
         key = key_entry.get()
-        key_entry.config(state=DISABLED)
+        key_entry.config(state=DISABLED)   
     def clearKey():
         global key
         key = ""
         key_entry.config(state=NORMAL)
         key_entry.delete(0,END)
     def clearPath():
+        path_entry.config(state=NORMAL)
         path_entry.delete(0,END)
     def browse_button():
         # Allow user to select a directory and store it in global var
         # called folder_path
+        path_entry.config(state=NORMAL)
         filename = filedialog.askdirectory()
-        folder_path = filename
         path_entry.delete(0,END)
-        path_entry.insert(0, folder_path)
+        path_entry.insert(0, filename)
+
+    def scan():
+        keySet()
+        Error_counter = 0
+        #checking if the input is good:
+        Error_counter += checkAPI()
+        Error_counter += checkPath()
+        
+        if Error_counter == 0:
+            begin_scan(path_entry.get())
+
+    #i am dividing the window to 2 main frames, top and bottom        
+    top_frame = Frame(window, width=width, height=450, bg="#161625")
+    top_frame.pack()
     
+    #marg from side
+    marg_frames = Label(top_frame, text="",width=50, bg="#161625")
+    marg_frames.pack(side=LEFT)
     #frame1
-    frame1 = Frame(window, width=900, height=500,bg='#161625')
+    frame1 = Frame(top_frame, width=900, height=450,bg='#161625')
     frame1.pack(side=LEFT, anchor=NW)
     frame1_1 = Frame(frame1, width=900, height=200, bg="#161625")
     frame1_1.pack(anchor=W)
@@ -61,6 +103,9 @@ def page_load():
     frame1_3 = Frame(frame1, width=900, height=100, bg="#161625")
     frame1_3.pack(anchor=W)
     #frame1_1 components
+    #marg
+    marg_down = Label(frame1_1, text="",bg='#161625',height=5)
+    marg_down.pack()
     key_label = Label(frame1_1, text="API Key here", font=('Arial', 20),bg='#161625', fg='White')
     key_label.pack(side=TOP, anchor=NW)
     key_entry = Entry(frame1_1, font=("Arial", 23),width= 40)
@@ -94,24 +139,43 @@ def page_load():
     clear_path_btn.pack(side=LEFT)
 
     #frame2
-    frame2 = Frame(window, width=650, height=500,bg='red')
+    frame2 = Frame(top_frame, width=650, height=500,bg='red')
     frame2.pack(side=LEFT, anchor=NW)
     #image
     global big_image
-    big_image = PhotoImage(file = "Antivirus-biglogo.png")
+    big_image = PhotoImage(file = "antiVirus-mainimg.png")
     canvas = Canvas(frame2,width = 400, height = 400, bg='#161625', highlightbackground = "#161625", highlightcolor= "#161625")
     canvas.create_image(200, 200, image = big_image)
     canvas.pack(side=RIGHT, anchor=N)
+    #margin - top
+    scan_marg_top = Label(frame1_3, text="", height=2, bg="#161625")
+    scan_marg_top.pack()
+    #margin - left
+    scan_marg_left = Label(frame1_3, text="", width=59, bg="#161625")
+    scan_marg_left.pack(side=LEFT)
+    #scan btn
+    scan_btn = Button(frame1_3, text="Scan", font=("Arial", 23) ,command=scan)
+    scan_btn.pack()
+
+    
+    bottom_frame = Frame(window, width=width, height=500, bg = "#161625")
+    bottom_frame.pack()
+    
+    global proggress_bar_label
+    proggress_bar_label = Label(bottom_frame, text="Scan Proggress", font=("ariel", 40),bg = "#161625", fg='white')
+    proggress_bar_label.pack()
+
+    global proggress_bar
+    proggress_bar = ttk.Progressbar(bottom_frame, orient='horizontal',mode='determinate',length=500)
+    proggress_bar.pack()
+
+    
+
 
 page_load()
 
-def scan():
-    pass
 
-
-
-
-def main():
+def begin_scan(path):
     
     global files_scanned
     files_scanned = 0
@@ -169,16 +233,22 @@ def main():
         }
         response = requests.get(url, headers=headers)
         analyze_response(response, path)
-        print(f"{files_scanned / number_of_files * 100}% done!")
-
-
-    # first = "C:/Users/ADMIN/Pictures/Camera Roll"
-    # number_of_files = count_files(first)
-    global key
-    print(key)
-
-    # folder_search(first)
-    # print("done scanning!")
+        proggress_bar["value"] = files_scanned / number_of_files * 100
+        window.update()
 
     
+    path_to_scan = f"{path}"
+    number_of_files = count_files(path_to_scan)
+    
+    
+    proggress_bar_label.pack()
+    proggress_bar.pack()
+    proggress_bar['value'] = 0
+    
+
+    folder_search(path_to_scan)
+    print("done scanning!")
+
+
+
 window.mainloop()
